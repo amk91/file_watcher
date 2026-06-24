eff17535476816d90f9c8324ce5d3a8cc021f470
amk91 <agostino.pisanelli@gmail.com>
2026-06-24 21:50:14 +0100

    Move init_tracing out of service and into common

--- common/src/lib.rs
+++ common/src/lib.rs
@@ -1,4 +1,41 @@
-pub mod config;
+use std::path::PathBuf;
+
+use tracing::{Level, error, level_filters::LevelFilter};
+use tracing_appender::non_blocking::WorkerGuard;
+use tracing_subscriber::{Layer, layer::SubscriberExt, util::SubscriberInitExt};
+
 pub mod app_paths;
+pub mod config;
 
 pub const APP_NAME: &str = "file-watcher";
+
+pub fn init_tracing(log_dir: &PathBuf) -> Option<WorkerGuard> {
+    if cfg!(debug_assertions) {
+        tracing_subscriber::fmt()
+            .with_max_level(Level::TRACE)
+            .init();
+
+        None
+    } else {
+        let file_appender = tracing_appender::rolling::daily(log_dir, "file_watcher_service.log");
+        let (non_blocking, guard) = tracing_appender::non_blocking(file_appender);
+
+        let file_layer = tracing_subscriber::fmt::layer()
+            .with_writer(non_blocking)
+            .with_filter(LevelFilter::INFO);
+
+        let registry = tracing_subscriber::registry().with(file_layer);
+
+        match tracing_journald::layer() {
+            Ok(layer) => {
+                registry.with(layer.with_filter(LevelFilter::TRACE)).init();
+            }
+            Err(err) => {
+                registry.init();
+                error!(?err, "Unable to register journalctl layer to tracing");
+            }
+        }
+
+        Some(guard)
+    }
+}
--- service/src/app.rs
+++ service/src/app.rs
@@ -32,7 +32,7 @@ impl App {
     pub fn new() -> App {
         let app_paths = AppPaths::new();
 
-        let tracing_guard = App::init_tracing(&app_paths.data_dir.join("log"));
+        let tracing_guard = common::init_tracing(&app_paths.data_dir.join("log"));
 
         let config = Config::init(
             &app_paths.config_path,
@@ -45,37 +45,6 @@ impl App {
             history_config: Arc::new(RwLock::new(config.history_config)),
             app_paths,
             _tracing_guard: tracing_guard,
-        }
-    }
-
-    fn init_tracing(log_dir: &PathBuf) -> Option<WorkerGuard> {
-        if cfg!(debug_assertions) {
-            tracing_subscriber::fmt()
-                .with_max_level(Level::TRACE)
-                .init();
-
-            None
-        } else {
-            let file_appender = tracing_appender::rolling::daily(log_dir, "file_watcher.log");
-            let (non_blocking, guard) = tracing_appender::non_blocking(file_appender);
-
-            let file_layer = tracing_subscriber::fmt::layer()
-                .with_writer(non_blocking)
-                .with_filter(LevelFilter::INFO);
-
-            let registry = tracing_subscriber::registry().with(file_layer);
-
-            match tracing_journald::layer() {
-                Ok(layer) => {
-                    registry.with(layer.with_filter(LevelFilter::TRACE)).init();
-                }
-                Err(err) => {
-                    registry.init();
-                    error!(?err, "Unable to register journalctl layer to tracing");
-                }
-            }
-
-            Some(guard)
         }
     }
 
